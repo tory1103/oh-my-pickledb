ca245f5608238e3b2e982f153ddd2b3cb7421447
tory1103 <adrian.rubiboy11@gmail.com>
2021-08-03 15:17:36 +0200

    Adds truncate function

---
+++ tests/test11.py
@@ -0,0 +1,21 @@
+from my_pickledb import PickleDB
+
+"""
+
+Simple test to truncate a key
+Functions used:
+    .truncate() -> Removes key value but not key itself
+
+
+Website: https://tory1103.github.io/oh-my-pickledb/
+Documentation: https://tory1103.github.io/oh-my-pickledb/docs.html
+Issues: https://github.com/tory1103/oh-my-pickledb/issues
+
+"""
+
+database = PickleDB("h.db", load=False)
+database.set("my_example", "value0")
+
+print(database.get("my_example"))
+database.truncate("my_example")
+print(database.get("my_example"))
