878a62feb264794011e20ccb8c2b3d142263a04a
tory1103 <adrian.rubiboy11@gmail.com>
2021-08-17 23:06:50 +0200

    Version 0.2.3

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="oh-my-pickledb",
-    version="0.2.2",
+    version="0.2.3",
     author="Adrián Toral",
     author_email="adriantoral@sertor.es",
     description="Oh-My-PickleDB is an open source key-value store using Python's json module",
