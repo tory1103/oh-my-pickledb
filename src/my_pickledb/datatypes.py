b661e247d91134fb9e21b3ae5f8808b30963bf23
tory1103 <adrian.rubiboy11@gmail.com>
2021-09-26 17:38:23 +0200

    Adds DataTypes

---
+++ src/my_pickledb/datatypes.py
@@ -0,0 +1,25 @@
+def isString(data): return type(data) == str
+
+
+def isInt(data): return type(data) == int
+
+
+def isFloat(data): return type(data) == float
+
+
+def isBytes(data): return type(data) == bytes
+
+
+def isBool(data): return type(data) == bool
+
+
+def isDictionary(data): return type(data) == dict
+
+
+def isList(data): return type(data) == list
+
+
+def isSet(data): return type(data) == set
+
+
+def isTuple(data): return type(data) == tuple
