d169c594a44384f1a9379c3e1d4c3c7d40c67247
tory1103 <adrian.rubiboy11@gmail.com>
2022-02-27 12:58:50 +0100

    Fixes import error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh: long_description = fh.read(
 
 setuptools.setup(
     name="oh-my-pickledb",
-    version="0.4.2-dev",
+    version="0.4.2",
     author="Adrián Toral",
     author_email="adriantoral@sertor.es",
     description="Oh-My-PickleDB is an open source key-value store using Python's json module",
